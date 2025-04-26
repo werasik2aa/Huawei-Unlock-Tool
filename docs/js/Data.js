2605a2be532649dc42bb2b9fa382248dfe1ce732
Huawei bootloader research <werasik2aa@yandex.ru>
2025-04-27 00:00:51 +0300

    Y530-U00

--- docs/js/Data.js
+++ docs/js/Data.js
@@ -61,6 +61,7 @@ const Devices = [
 	"TOR-A1(FB_NOTEST)'https://raw.githubusercontent.com/werasik2aa/UnlockedHuaweiBootloader/main/TOR-A1.zip",
 	"TRT-XXX(FB_NOTEST)'https://raw.githubusercontent.com/werasik2aa/UnlockedHuaweiBootloader/main/TRT-XXX.zip",
 	"T1-A21L(FB_NOTEST)'https://raw.githubusercontent.com/werasik2aa/UnlockedHuaweiBootloader/main/T1-A21L.zip",
+	"Y530-U00(FULL)'https://raw.githubusercontent.com/werasik2aa/UnlockedHuaweiBootloader/main/Y530-U00.zip",
 	"Y330-C00(FULL)'https://raw.githubusercontent.com/werasik2aa/UnlockedHuaweiBootloader/main/Y330-C00.zip",
 	"Y635-L01(FULL)'https://raw.githubusercontent.com/werasik2aa/UnlockedHuaweiBootloader/main/Y635-L01.zip",
 	"Y635-L02(FULL)'https://raw.githubusercontent.com/werasik2aa/UnlockedHuaweiBootloader/main/Y635-L02.zip",
